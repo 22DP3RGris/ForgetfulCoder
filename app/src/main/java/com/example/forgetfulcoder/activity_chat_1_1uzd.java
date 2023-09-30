056b6aceccd505e7f2e07e9e2228549f19a94f41
Rinalds Zelčs <rz00754@rvt.lv>
2023-09-30 16:47:22 +0300

    First lvl something

--- app/src/main/java/com/example/forgetfulcoder/activity_chat_1_1uzd.java
+++ app/src/main/java/com/example/forgetfulcoder/activity_chat_1_1uzd.java
@@ -2,7 +2,9 @@ package com.example.forgetfulcoder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class activity_chat_1_1uzd extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class activity_chat_1_1uzd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat11uzd);
+        Button compiler = (Button)findViewById(R.id.goToComp);
+
+        compiler.setOnClickListener(view -> {
+            Intent i = new Intent(activity_chat_1_1uzd.this, Compiler.class);
+            startActivity(i);
+        });
     }
 }
--- app/src/main/java/com/example/forgetfulcoder/category1/cat_1_level.java
+++ app/src/main/java/com/example/forgetfulcoder/category1/cat_1_level.java
@@ -2,10 +2,14 @@ package com.example.forgetfulcoder.category1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.example.forgetfulcoder.MainActivity;
 import com.example.forgetfulcoder.R;
+import com.example.forgetfulcoder.activity_chat_1_1uzd;
 
 public class cat_1_level extends AppCompatActivity {
     @Override
@@ -13,6 +17,11 @@ public class cat_1_level extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat1_level);
         ImageView back = (ImageView)findViewById(R.id.backBtn);
+        Button lvl1 = (Button)findViewById(R.id.cat1Level1);
+
+        lvl1.setOnClickListener(view -> {Intent i = new Intent(cat_1_level.this, activity_chat_1_1uzd.class); startActivity(i);});
+        
         back.setOnClickListener(view -> finish());
+
     }
 }
