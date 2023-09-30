91a83e6dd1b2958eec55ede074304831daff0800
Roberts Grišins <rg00220@rvt.lv>
2023-09-30 12:04:24 +0300

    Idk

---
+++ app/src/main/java/com/example/forgetfulcoder/Compiler.java
@@ -0,0 +1,15 @@
+package com.example.forgetfulcoder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Compiler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compiler);
+
+    }
+}
