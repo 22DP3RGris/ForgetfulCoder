0d308c3acef7ae4a49441754ff49baf0b189a5e0
Rinalds Zelčs <rz00754@rvt.lv>
2023-09-30 17:10:20 +0300

    Working back buttons for settings and leaderboard

--- app/src/main/java/com/example/forgetfulcoder/leaderboardActivity.java
+++ app/src/main/java/com/example/forgetfulcoder/leaderboardActivity.java
@@ -3,6 +3,7 @@ package com.example.forgetfulcoder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class leaderboardActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class leaderboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+        ImageView back = (ImageView)findViewById(R.id.backBtn);
+
+        back.setOnClickListener(view -> finish());
     }
 }
--- app/src/main/java/com/example/forgetfulcoder/settingsActivity.java
+++ app/src/main/java/com/example/forgetfulcoder/settingsActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class settingsActivity extends AppCompatActivity {
 
@@ -13,6 +14,9 @@ public class settingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ImageView back = (ImageView)findViewById(R.id.backBtn);
+
+        back.setOnClickListener(view -> finish());
 
     }
 }
