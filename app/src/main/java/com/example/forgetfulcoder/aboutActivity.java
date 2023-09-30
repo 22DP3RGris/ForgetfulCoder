4f3d990bf08bc8611a0d9668d2d17e2b26e91894
Robert <54269637+R-udren@users.noreply.github.com>
2023-09-30 18:21:18 +0300

    Back button to About

--- app/src/main/java/com/example/forgetfulcoder/aboutActivity.java
+++ app/src/main/java/com/example/forgetfulcoder/aboutActivity.java
@@ -2,6 +2,7 @@ package com.example.forgetfulcoder;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class aboutActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class aboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ImageView back = (ImageView)findViewById(R.id.backBtn);
+        back.setOnClickListener(view -> finish());
     }
 }
