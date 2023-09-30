e3a90866fd8920c56f373d25e9df78fe862325a6
Renarts_Gelze-Veiss <renarts.gv@gmail.com>
2023-09-30 16:34:23 +0300

    Nomainiju krasu nosaukumam

---
+++ app/src/main/java/com/example/forgetfulcoder/activity_chat_1_1uzd.java
@@ -0,0 +1,14 @@
+package com.example.forgetfulcoder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_chat_1_1uzd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat11uzd);
+    }
+}
