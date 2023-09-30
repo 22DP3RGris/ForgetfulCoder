d1cb1d79c64fb3a8c30f0e897466a8c52025ed3e
Rinalds Zelčs <rz00754@rvt.lv>
2023-09-30 14:16:48 +0300

    Category 1 Level back button works

--- app/src/main/java/com/example/forgetfulcoder/category1/cat_1_level.java
+++ app/src/main/java/com/example/forgetfulcoder/category1/cat_1_level.java
@@ -3,6 +3,10 @@ package com.example.forgetfulcoder.category1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.forgetfulcoder.R;
 
@@ -12,5 +16,12 @@ public class cat_1_level extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat1_level);
+        ImageView back = (ImageView)findViewById(R.id.backBtn);
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
