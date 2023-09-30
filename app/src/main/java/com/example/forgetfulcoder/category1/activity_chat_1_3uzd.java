7bbc694c7a134c92928264d9c0d18ff24aa40b50
Roberts Grišins <rg00220@rvt.lv>
2023-10-01 02:38:36 +0300

    Cat 1 chat 3 without compiler finished

---
+++ app/src/main/java/com/example/forgetfulcoder/category1/activity_chat_1_3uzd.java
@@ -0,0 +1,16 @@
+package com.example.forgetfulcoder.category1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.forgetfulcoder.R;
+
+public class activity_chat_1_3uzd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat13uzd);
+    }
+}
