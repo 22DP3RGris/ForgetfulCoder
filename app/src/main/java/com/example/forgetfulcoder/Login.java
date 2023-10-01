774f0c1ab56595562a2020ba6ff1b11eaa056078
Herberts Eisulis <h.eisulis@gmail.com>
2023-10-01 06:20:13 +0300

    Login and Register system

--- app/src/main/java/com/example/forgetfulcoder/Login.java
+++ app/src/main/java/com/example/forgetfulcoder/Login.java
@@ -1,18 +1,102 @@
 package com.example.forgetfulcoder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+
+    public void Notify(String reason) {
+        TextView ErrorMessage = (TextView) findViewById(R.id.errornotify);
+
+        ErrorMessage.setText(reason);
+        ErrorMessage.setVisibility(View.VISIBLE);
+        ErrorMessage.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ErrorMessage.setVisibility(View.GONE);
+
+            }
+        }, 2 * 1000);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ImageView back = (ImageView)findViewById(R.id.backBtn);
-        back.setOnClickListener(view -> finish());
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://forgetful-coder-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference Ref = database.getReference("/UserData");
+
+        Button LoginButton = (Button)findViewById(R.id.loginButton);
+        Button toRegister = (Button)findViewById(R.id.toRegister);
+        EditText Username = (EditText)findViewById(R.id.usernameLog);
+        EditText Password = (EditText)findViewById(R.id.passwordLog);
+        sharedPreferences = getSharedPreferences("ForgetfulCoder", MODE_PRIVATE);
+
+        if (sharedPreferences.getString("logged", "false").equals("true")) {
+            Intent i = new Intent(Login.this, MainActivity.class);
+            startActivity(i);
+        }
+
+        LoginButton.setOnClickListener(view -> {
+            String username = Username.getText().toString();
+            String password = Password.getText().toString();
+
+            if (username != null && password != null) {
+                Ref.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (!task.isSuccessful()) {
+                            Log.e("firebase", "Error getting data", task.getException());
+                        } else {
+                            Object value = task.getResult().child(username).getValue();
+
+                            if (value != null) {
+                                Map<String, Object> datas = (Map<String, Object>) value;
+                                if (password.equals(datas.get("password"))) {
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString("logged", "true");
+                                    editor.putString("name", username);
+                                    editor.putString("email", datas.get("email").toString());
+                                    editor.apply();
+
+                                    Intent i = new Intent(Login.this, MainActivity.class);
+                                    startActivity(i);
+                                }
+                            }
+                        }
+                    }
+                });
+            } else if (username == null) {
+                Notify("Invalid username");
+            } else if (password == null) {
+                Notify("Invalid password");
+            }
+        });
+
+        toRegister.setOnClickListener(view -> {
+            Intent i = new Intent(Login.this, Register.class);
+            startActivity(i);
+        });
     }
 }
--- app/src/main/java/com/example/forgetfulcoder/Register.java
+++ app/src/main/java/com/example/forgetfulcoder/Register.java
@@ -1,18 +1,139 @@
 package com.example.forgetfulcoder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Type;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Register extends AppCompatActivity {
+
+    public class User {
+
+        public String email;
+        public String password;
+
+        public User(String Email,String Password) {
+            this.email = Email;
+            this.password = Password;
+        }
+    }
+
+    public static Boolean ValidateEmail(String email) {
+        String PatternRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+        Pattern pat = Pattern.compile(PatternRegex);
+        if (email == null) {
+            return false;
+        }
+
+        Boolean matches = pat.matcher(email).matches();
+        return matches;
+    }
+
+    public void Notify(String reason) {
+        TextView ErrorMessage = (TextView) findViewById(R.id.errornotify);
+
+        ErrorMessage.setText(reason);
+        ErrorMessage.setVisibility(View.VISIBLE);
+        ErrorMessage.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ErrorMessage.setVisibility(View.GONE);
+
+            }
+        }, 2 * 1000);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://forgetful-coder-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference Ref = database.getReference("/UserData");
+
         ImageView back = (ImageView)findViewById(R.id.backBtn);
+        TextView registerbutton = (Button)findViewById(R.id.registerBtn);
+
+        registerbutton.setOnClickListener(view -> {
+            EditText Username = (EditText)findViewById(R.id.usernameReg);
+            EditText Email = (EditText)findViewById(R.id.emailReg);
+            EditText Password = (EditText)findViewById(R.id.passwordReg);
+            EditText ConPassword = (EditText)findViewById(R.id.passwordConReg);
+
+            String username = Username.getText().toString();
+            String email = Email.getText().toString();
+            String password = Password.getText().toString();
+            String confirmpass = ConPassword.getText().toString();
+
+            Boolean EmailValid = ValidateEmail(email);
+
+            if (EmailValid && username != null && password != null && confirmpass.equals(password)) {
+
+                Ref.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (!task.isSuccessful()) {
+                            Log.e("firebase", "Error getting data", task.getException());
+                        } else {
+
+                            Object value = task.getResult().child(username).getValue();
+
+                            if (value == null) {
+                                for (DataSnapshot dsp : task.getResult().getChildren()) {
+                                    Map<String, Object> datas = (Map<String, Object>) dsp.getValue();
+                                    if (email.equals(datas.get("email"))) {
+                                        Notify("This email is already in use");
+                                        return;
+                                    }
+                                }
+
+                                User user = new User(email, password);
+
+                                Ref.child(username).setValue(user);
+
+                                Intent i = new Intent(Register.this, Login.class);
+                                startActivity(i);
+                            } else {
+                                Notify("This username is already in use");
+                            }
+                        }
+                    }
+                });
+            } else if (!EmailValid) {
+                Notify("Invalid email");
+            } else if (username == null) {
+                Notify("Invalid username");
+            } else if (password == null) {
+                Notify("Invalid password");
+            } else if (!confirmpass.equals(password)) {
+                Notify("Passwords do not match");
+            }
+        });
+
         back.setOnClickListener(view -> finish());
     }
 }
