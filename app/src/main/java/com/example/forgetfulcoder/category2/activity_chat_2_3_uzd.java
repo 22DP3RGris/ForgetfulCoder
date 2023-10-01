e33bb492a45ad756309f34cf8216abdb4a7ebefd
Roberts Grišins <rg00220@rvt.lv>
2023-10-01 04:31:42 +0300

    Rinaldam

---
+++ app/src/main/java/com/example/forgetfulcoder/category2/activity_chat_2_3_uzd.java
@@ -0,0 +1,16 @@
+package com.example.forgetfulcoder.category2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.forgetfulcoder.R;
+
+public class activity_chat_2_3_uzd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat23_uzd);
+    }
+}
